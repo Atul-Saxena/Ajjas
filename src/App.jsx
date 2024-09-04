23bba1a0f4a75729327eeda97185ade910b37d4e
Sakshi-Saxena <saxenasakshi1105@gmail.com>
2024-09-04 15:26:31 +0530

    votes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,24 @@ import './App.css'
 function App(space = 0) {
   const [Comments, SetComments] = useState([]);
   const [commentVal, setCommentVal] = useState("");
+  const [votes, setVotes] = useState(0);
 
   const AddComments = () => {
     SetComments((PrevComments) => [...PrevComments, commentVal]);
   }
 
+  const UpVote = ()=>{
+    setVotes((val)=>val+1);
+  }
+
+  const DownVote = ()=>{
+    setVotes((val)=>val-1);
+  }
+
   return (
     <>
       <button onClick={AddComments}>
-        Add Comments
+        Add Comment
       </button>
       <input type="text" placeholder='Comments?' value={commentVal} onChange={(e) => (
         setCommentVal(e.target.value)
@@ -21,6 +30,9 @@ function App(space = 0) {
       {Comments && Comments.map((value)=>(
         <div className='element'>
           <h4>{value}</h4>
+          <button onClick={UpVote}>upvote</button>
+          <button onClick={DownVote}>downvote</button>
+          <p>{votes}</p>
           <App space={space+1}/>
         </div>
       ))}
